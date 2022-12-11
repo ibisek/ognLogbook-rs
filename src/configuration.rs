3545b34ee373e648e192bf68801a413d2221bcb8
ibisek <ibisek@protonmail.com>
2022-12-11 19:27:29 +0100

    added db config

--- src/configuration.rs
+++ src/configuration.rs
@@ -10,6 +10,22 @@ pub const GEOTIFF_FILEPATH: &str = "./data/mosaic-500m.TIF";
 
 pub const AIRFIELDS_FILEPATH: &str = "./data/airfields.json";
 
+const DB_HOST: &str = "localhost";
+const DB_PORT: &str = "3306";
+const DB_NAME: &str = "ogn_logbook";
+const DB_USER: &str = "ibisek";
+const DB_PASSWORD: &str = "**";
+pub fn get_db_url() -> String {
+    let db_host = env::var("DB_HOST").unwrap_or(DB_HOST.into());
+    let db_port = env::var("DB_PORT").unwrap_or(DB_PORT.into());
+    let db_name = env::var("DB_NAME").unwrap_or(DB_NAME.into());
+    let db_user = env::var("DB_USER").unwrap_or(DB_USER.into());
+    let db_password = env::var("DB_PASSWORD").unwrap_or(DB_PASSWORD.into());
+    // url format: "mysql://user:password@host:port/db_name"
+    format!("mysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}")
+}
+
+
 pub const REDIS_RECORD_EXPIRATION: usize = 8*60*60;   // [s]
 
 const REDIS_URL: &str = "redis://127.0.0.1:6379/";
