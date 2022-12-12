9c6b059d404dc51e5cec0ddd4a984b08deb30dcd
ibisek <ibisek@protonmail.com>
2022-12-12 21:02:25 +0100

    work in progress

---
+++ src/worker/utils.rs
@@ -0,0 +1,19 @@
+
+use ogn_client::data_structures::AircraftType;
+
+/**
+ * :param AircraftType
+ * :param forEvent: 'L' / 'T' threshold for expected event
+ * :return: threshold speed for given event in km/h
+ */
+pub fn get_groundspeed_threshold(aircraft_type: &AircraftType, for_event: char) -> f64 {
+    if for_event == 'L' {
+        if vec![AircraftType::Glider, AircraftType::Helicopter, AircraftType::Parachute, AircraftType::HangGlider, AircraftType::Paraglider, AircraftType::Baloon, AircraftType::Airship, AircraftType::Uav].contains(aircraft_type) {
+            return 20_f64;  // [km/h] glider]
+        } else {
+            return 50_f64;  //[km/h] tow
+        }
+    } else {    // takeoff threshold
+        return 80_f64;   // [km/h] all
+    }
+}
