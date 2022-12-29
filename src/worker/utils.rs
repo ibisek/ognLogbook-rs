b5804f6496a52b9ef7fbe6d908ca4b515b931fcf
ibisek <ibisek@protonmail.com>
2022-12-29 08:33:14 +0100

    much faster

--- src/worker/utils.rs
+++ src/worker/utils.rs
@@ -1,5 +1,6 @@
-
 use ogn_client::data_structures::AircraftType;
+
+static SLOW_CRAFTS: [AircraftType; 8] = [AircraftType::Glider, AircraftType::Helicopter, AircraftType::Parachute, AircraftType::HangGlider, AircraftType::Paraglider, AircraftType::Baloon, AircraftType::Airship, AircraftType::Uav];
 
 /**
  * :param AircraftType
@@ -8,7 +9,7 @@ use ogn_client::data_structures::AircraftType;
  */
 pub fn get_groundspeed_threshold(aircraft_type: &AircraftType, for_event: char) -> f64 {
     if for_event == 'L' {
-        if vec![AircraftType::Glider, AircraftType::Helicopter, AircraftType::Parachute, AircraftType::HangGlider, AircraftType::Paraglider, AircraftType::Baloon, AircraftType::Airship, AircraftType::Uav].contains(aircraft_type) {
+        if SLOW_CRAFTS.contains(aircraft_type) {
             return 20_f64;  // [km/h] glider]
         } else {
             return 50_f64;  //[km/h] tow
