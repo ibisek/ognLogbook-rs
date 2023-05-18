449250cba959beef4680ba1b29d6edd6b37706c5
ibisek <ibisek@protonmail.com>
2023-05-18 20:10:27 +0200

    better handling db connection error

--- src/db/mysql.rs
+++ src/db/mysql.rs
@@ -1,6 +1,6 @@
 
-use log::debug;
-use mysql::{Pool, PooledConn};
+use log::{debug, error};
+use mysql::{Pool, PooledConn, Error};
 
 
 use crate::configuration::get_db_url;
@@ -11,15 +11,24 @@ pub struct MySQL {
 
 impl MySQL {
 
-    pub fn new() -> MySQL {
+    pub fn new() -> Result<MySQL, Error> {
         let binding = get_db_url();
         let db_url = binding.as_str();
-        let pool = Pool::new(db_url).expect("Could not connect to MySQL db!");
+        // let pool = Pool::new(db_url).expect("Could not connect to MySQL db!");
+        let pool = Pool::new(db_url);
 
-        debug!("MySQL at {db_url}");
-
-        MySQL {
-            pool,
+        match pool {
+            Ok(pool) => {
+                debug!("MySQL at {db_url}");
+                
+                Ok(MySQL {
+                    pool,
+                })
+            },
+            Err(e) => {
+                error!("Could not connect to MySQL db!");
+                Err(e)
+            },
         }
     }
 
